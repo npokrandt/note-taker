9221746a40cedc454eec6676ce6f968679cd6a60
npokrandt <npokrandt919@gmail.com>
2023-08-15 09:28:29 -0500

    set up the basic server and added some eventual route files

---
+++ server.js
@@ -0,0 +1,13 @@
+//port, express, something else and imports
+const express = require('express')
+const app = express()
+const PORT = 2024
+
+//middleware
+
+//things
+
+//listening on port this
+app.listen(PORT, () =>{
+    console.log(`Note Taker listening on http://localhost:${PORT}`)
+})
